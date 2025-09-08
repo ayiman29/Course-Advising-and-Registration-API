742cff7f02ece9ab2dac992804937d81d3da9218
RZ Ayiman <ayimaneg@gmail.com>
2025-09-08 20:49:11 +0600

    Fixed port numbers

--- db.js
+++ db.js
@@ -1,22 +1,26 @@
 import mysql from 'mysql2';
-import dotenv from 'dotenv';
-dotenv.config();
+
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT || 3306),
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME,           // university5
+  host: 'localhost',
+  user: 'root',        
+  password: '',       
+  database: 'university5', 
   waitForConnections: true,
   connectionLimit: 10,
+  queueLimit: 0
 });
+
 
 const db = pool.promise();
 
 export async function pingDB() {
-  const [rows] = await db.query('SELECT DATABASE() AS db, NOW() AS now');
-  console.log('✅ Connected to DB:', rows[0].db, 'at', rows[0].now);
+  try {
+    const [rows] = await db.query('SELECT DATABASE() AS db, NOW() AS now');
+    console.log('✅ Connected to DB:', rows[0].db, 'at', rows[0].now);
+  } catch (err) {
+    console.error('❌ DB connection failed:', err.message);
+  }
 }
 
 export default db;
