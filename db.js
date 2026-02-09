ea4b5e43bc07c73765167995a99d0e41fe1ee276
ayiman29 <ayimaneg@gmail.com>
2026-02-09 20:15:23 +0600

    sqldump fixed

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ import mysql from 'mysql2';
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',        
-  password: '',       
+  password: 'A123b456',       
   database: 'university5', 
   waitForConnections: true,
   connectionLimit: 10,
